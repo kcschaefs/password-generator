35e47adbde27caa28d2546727813424b56d36f15
Kristen Schaefer <kristencschaefer@gmail.com>
2022-12-21 17:29:14 -0600

    added global vars, arrays, and functions

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// this is where I need to enter the vars, arrays, and loops
+var addSpecialChar = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ":", ";", "'", "<", ">", "?"];
+var addLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var addUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var addNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var numberOfCharacters;
+var useSpecialChar;
+var useLowerCase;
+var useUpperCase;
+var useNumbers;
+var finishedPassword = "" ;
+
+function askNumOfChar () {
+  var response = prompt ("How many characters would you like your password to be?");
+  numberOfCharacters = parseInt (response);
+}
+
+function askUseSpecialChar () {
+  var response = confirm ("Is it ok to use special characters?");
+}
+
+function askUseLowerCase () {
+  var response = confirm ("Is it ok to use lower case letters?");
+}
+
+function askUseUpperCase () {
+  var response = confirm ("Is it ok to use upper case letters?");
+}
+
+function askUseNumbers () {
+  var response = confirm ("Is it ok to use numbers?");
+}
+
+// --------------------------------------------------------------
+
+
+  console.log(askNumOfChar())
+  console.log(askUseSpecialChar());
+  console.log(askUseLowerCase());
+  console.log(askUseUpperCase());
+  console.log(askUseNumbers());
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
