0af2f5309ebac5b932f92a2293716222836c9187
Kristen Schaefer <kristencschaefer@gmail.com>
2022-12-22 14:11:00 -0600

    added confirmation responses to questions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// -------------------------------------------------------------
+
 var addSpecialChar = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ":", ";", "'", "<", ">", "?"];
 var addLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var addUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -21,66 +23,70 @@ function askNumOfChar () {
 
 function askUseSpecialChar () {
   var response = confirm ("Is it ok to use special characters?");
+  if (response ==true) {
+    useSpecialChar =true;
+  } else useSpecialChar=false;
 }
 
 function askUseLowerCase () {
   var response = confirm ("Is it ok to use lower case letters?");
+  if (response ==true) {
+    useLowerCase =true;
+  } else useLowerCase=false;
 }
 
 function askUseUpperCase () {
   var response = confirm ("Is it ok to use upper case letters?");
+  if (response ==true) {
+    useUpperCase =true;
+  } else useUpperCase=false;
 }
 
 function askUseNumbers () {
   var response = confirm ("Is it ok to use numbers?");
+  if (response ==true) {
+    useNumbers =true;
+  } else useNumbers=false;
 }
 
-function generatePassword (){
-  var numberOfCharacters = 12;
+function generatePassword () {
+ askNumOfChar()
+  askUseSpecialChar()
+  //var numberOfCharacters = 12;
   var charsToUse = []; //need to create an empty array to allow for the user character selection
-
-//  charsToUse = charsToUse.concat (addLowerCase);
-//  charsToUse = charsToUse.concat (addUpperCase);
-//  charsToUse = charsToUse.concat (addNumbers);
-//  charsToUSe = charsToUse.concat (addSpecialChar);
+  charsToUse = charsToUse.concat (addLowerCase); //adds lowercase to array
+  charsToUse = charsToUse.concat (addUpperCase); //adds uppercase to array
+  charsToUse = charsToUse.concat (addNumbers); //adds numbers to array
+  charsToUse = charsToUse.concat (addSpecialChar); //adds spec characters to array
+  // this has added all chars types to array, need to figure out how to pick and choose
  //var charsToUse = addLowerCase.concat (addUpperCase, addNumbers, addSpecialChar); //don't want to do this, bc it hardcodes ALL arrays and doesn't allow for user rejection of array
- console.log(charsToUse);
+  console.log(useSpecialChar);
 
-
+// } else {
+//     var charsToUse = []; //need to create an empty array to allow for the user character selection
+//   charsToUse = charsToUse.concat (addLowerCase); //adds lowercase to array
+//   charsToUse = charsToUse.concat (addUpperCase); //adds uppercase to array
+//   charsToUse = charsToUse.concat (addNumbers);
+//   console.log(askUseSpecialChar(false)) ;
+   
   var password = "";
-
+  
+  
   for (var i =0 ; i < numberOfCharacters; i++) {
     var randomNumber = Math.floor(Math.random() * charsToUse.length); //chooses a random number to apply to the array
-    console.log(randomNumber);
     password += charsToUse [randomNumber];
-    console.log(password);
   }
-
+ 
   return password;
-
+  
+ }
   //askNumOfChar();
   //askUseLowerCase();
   //askUseUpperCase();
   //askUseSpecialChar();
   //askUseNumbers();
-}
+
 // --------------------------------------------------------------
-
-
-
- // console.log(askNumOfChar())
-  //console.log(askUseSpecialChar());
-  //console.log(askUseLowerCase());
-  //console.log(askUseUpperCase());
-  //console.log(askUseNumbers());
-
-
-for (var i =0 ; i < numberOfCharacters; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring (randomNumber, randomNumber+1);
-}
-
-
 
 // Write password to the #password input
 function writePassword() {
